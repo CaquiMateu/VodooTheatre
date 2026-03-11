5f103a82814c3c4858c88fb57ba99e627c607b11
CaquiMateu <amateua@esimurcia.com>
2026-03-11 13:53:02 +0100

    +

--- Vodoo Theatre/Assets/Scripts/FlyingEnemy.cs
+++ Vodoo Theatre/Assets/Scripts/FlyingEnemy.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class FlyingEnemy : MonoBehaviour
 {
+    private AIDestinationSetter setter;
+
+    public Transform[] puntos;
+    public int ÍndiceActual = 0;
+
+    public bool chasing = false;
+
+    private void Start()
+    {
+        setter = GetComponent<AIDestinationSetter>();
+        setter.target = puntos[ÍndiceActual];
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") == true)
         {
+            chasing = true;
             Debug.Log("Goo Goo Gaa Gaaa");
+            setter.target = collision.transform;
+        }
+    }
+    void Update()
+    {
+        
+        if (Vector3.Distance(transform.position, puntos[ÍndiceActual].position) <= 0.01f && chasing==false)
+        {
+            ÍndiceActual = Random.Range(0, puntos.Length);
+
+            setter.target = puntos[ÍndiceActual];
         }
     }
 }
