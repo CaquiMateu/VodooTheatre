273b7c1cd8daa1cca6bb9d12c6daf4f54b8b77a7
CaquiMateu <amateua@esimurcia.com>
2026-03-13 10:28:06 +0100

    Disparo Enemigo

--- Vodoo Theatre/Assets/Scripts/FlyingEnemy.cs
+++ Vodoo Theatre/Assets/Scripts/FlyingEnemy.cs
@@ -6,15 +6,21 @@ using Pathfinding;
 public class FlyingEnemy : MonoBehaviour
 {
     private AIDestinationSetter setter;
+    private AIPath Path;
 
     public Transform[] puntos;
     public int ÍndiceActual = 0;
 
     public bool chasing = false;
+    public float attackDinstance = 2;
+    public GameObject BalaPrefab;
+    public float RecargaBala = 1;
+    public float VelocidadBala = 3;
 
     private void Start()
     {
         setter = GetComponent<AIDestinationSetter>();
+        Path = GetComponent<AIPath>();
         setter.target = puntos[ÍndiceActual];
     }
 
@@ -29,12 +35,29 @@ public class FlyingEnemy : MonoBehaviour
     }
     void Update()
     {
+        if (setter.target.position.x > transform.position.x ) 
+        {
+            this.transform.localScale = Vector3.one;
+        }
+        else
+        {
+            transform.localScale = new Vector3 (-1,1,1);
+        }
         
-        if (Vector3.Distance(transform.position, puntos[ÍndiceActual].position) <= 0.01f && chasing==false)
+        if (Vector3.Distance(transform.position, puntos[ÍndiceActual].position) <= 0.1f && chasing==false)
         {
             ÍndiceActual = Random.Range(0, puntos.Length);
 
             setter.target = puntos[ÍndiceActual];
+
+            Path.endReachedDistance = attackDinstance;
         }
+    }
+
+    void shoot()
+    {
+        GameObject Bala = Instantiate(BalaPrefab, new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity);
+        Vector3 PlayerDirection = setter.target.position - transform.position;
+        Bala.GetComponent<Rigidbody2D>().AddForce(PlayerDirection.normalized * VelocidadBala, ForceMode2D.Impulse);
     }
 }
