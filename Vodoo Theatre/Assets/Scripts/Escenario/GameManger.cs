3247c81b473d45b8db90386c6736a6e4b30df8ac
CaquiMateu <amateua@esimurcia.com>
2026-04-13 12:51:16 +0200

    CambioDeEscenario

--- Vodoo Theatre/Assets/Scripts/Escenario/GameManger.cs
+++ Vodoo Theatre/Assets/Scripts/Escenario/GameManger.cs
@@ -2,22 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Cinemachine;
 
 public class GameManger : MonoBehaviour
 {
    public PlayerController PlayerController;
    public SpawnEnemigos SpawnEnemigos;
-
+   public CinemachineConfiner2D CinemachineConfiner2D;
+    public Collider2D CámaraDía;
+    public Collider2D CámaraNoche;
+    private void Start()
+    {
+        CambioDecámara(CámaraDía);
+    }
+    public void CambioDecámara(Collider2D NewCofinder)
+    {
+        if (CinemachineConfiner2D.m_BoundingShape2D != NewCofinder) 
+        {
+            CinemachineConfiner2D.m_BoundingShape2D = NewCofinder;
+        }
+    }
    
 
     public void CambioDeEscenarioANoche()
     {
         PlayerController.CambioDeZonaANoche();
-
+        CambioDecámara(CámaraNoche);
     }
+
+    
     public void CambioDeEscenarioADia()
     {
+       
         PlayerController.CambioDeZonaADia();
+        CambioDecámara(CámaraDía);
         SpawnEnemigos.NumSpawnDificultad();
         SpawnEnemigos.HordaAcabada = false;
     }
